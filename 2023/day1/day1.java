48339d86d8a3ba468643369ccc4ea46d8ab29948
Marco <marco.a.romero0303@gmail.com>
2023-12-11 23:44:43 -0800

    day 1 part 1 is done

--- 2023/day1/day1.java
+++ 2023/day1/day1.java
@@ -9,8 +9,7 @@ public class day1{
         String filePath = "./2023/day1/day1input.txt";
 
         // testing file with only 10 lines
-        //String filePath = "./2023/day1/day1input-copy.txt";
-
+        // String filePath = "./2023/day1/day1input-copy.txt";
         try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
             String line = "";
 
@@ -22,12 +21,13 @@ public class day1{
         } catch(IOException e){
             System.out.println("error: " + e);
         }
+        System.out.println(finalSum);
     }
 
     // this function will process each line and will determine which numbers are what 
     private static void processLine(String fileLine){
-        String firstNum = "0";
-        String lastNum = "0";
+        String firstNum = "";
+        String lastNum = "";
         String newNum = "";
         int numStringToInt = 0;
         int endOfLine = fileLine.length() - 1;
@@ -38,7 +38,7 @@ public class day1{
 
             // if letter is a digit and the firstNum is still original value, then change value
             // of firstNum
-            if(Character.isDigit(letter) && firstNum == "0"){
+            if(Character.isDigit(letter) && firstNum == "" && i != endOfLine){
                 firstNum = Character.toString(letter);
             }
 
@@ -46,37 +46,31 @@ public class day1{
             // of the line, then change value of the lastNum
             // we are checking that we are not end of the line because it skips the logic/formula of
             // adding the nums together and adding it all up 
-            else if(Character.isDigit(letter) && firstNum != "0" && i != endOfLine){
+            else if(Character.isDigit(letter) && firstNum != "" && i != endOfLine){
                 lastNum = Character.toString(letter);
             }
 
             // if we reach the end of this line, we know to start adding nums together
             else if(i == endOfLine){
-                // if we havent gotten a second num, then make it equal firstNum
-                if(lastNum == "0" && !Character.isDigit(letter)){
+                // check if firstNum was still not found and we are at the last char of the line
+                if(firstNum == "" && Character.isDigit(letter)){
+                    firstNum = Character.toString(letter);
                     lastNum = firstNum;
-                    newNum = firstNum + lastNum;
-                    numStringToInt = Integer.parseInt(newNum);
-                    finalSum += numStringToInt;
-
                 }
-
+                // if we havent gotten a second num, then make it equal firstNum
+                else if(lastNum == "" && !Character.isDigit(letter)){
+                    lastNum = firstNum;
+                }
                 // if last letter is a num, and we already found the first num, then do this
-                else if(Character.isDigit(letter) && firstNum != "0"){
+                else if(Character.isDigit(letter) && firstNum != ""){
                     lastNum = Character.toString(letter);
-                    newNum = firstNum + lastNum;
-                    numStringToInt = Integer.parseInt(newNum);
-                    finalSum += numStringToInt;
                 }
 
-                // if both lastNum and firstNum are found then do this
-                else{
-                    newNum = firstNum + lastNum;
-                    numStringToInt = Integer.parseInt(newNum);
-                    finalSum += numStringToInt;
-                }
+                // concatenation of first and last num, turning it into an int, then adding it all up
+                newNum = firstNum + lastNum;
+                numStringToInt = Integer.parseInt(newNum);
+                finalSum = finalSum + numStringToInt;
             }
         }
-        System.out.println(finalSum);
     }
 }
