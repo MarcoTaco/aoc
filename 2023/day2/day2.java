7f81338eb0da95038f6b2637c909d45722698c4c
Marco <marco.a.romero0303@gmail.com>
2023-12-23 11:23:32 -0800

    progress on day2 problem

--- 2023/day2/day2.java
+++ 2023/day2/day2.java
@@ -3,8 +3,12 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class day2 {
+    public static int finalAnswer = 0;
     public static void main(String[] args){
-        String file = "./2023/day2/day2input.txt";
+        //String file = "./2023/day2/day2input.txt";
+
+        // test file
+        String file = "./2023/day2/day2input-test.txt";
 
         try(BufferedReader reader = new BufferedReader(new FileReader(file))){
             String line = "";
@@ -23,8 +27,15 @@ public class day2 {
         int blueCount = 0;
         int greenCount = 0;
 
-        for(int i = 0; i < fileLine.length(); i++){
-            
-        }
+        // splitting everything into two sections. splitting in between the colon
+        String[] sections = fileLine.split(":");
+
+        // sections[0] is everything before the colon, and only grabbing the id number
+        String numBefore = sections[0].replaceAll("[^0-9]", "");
+
+        // sections[1] is everything after the colon
+        String numAfter = sections[1];
+
+        
     }
 }
