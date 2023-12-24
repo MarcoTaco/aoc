84a1eb112c70bc24b2fdd355321f5c057d0dcff8
Marco <marco.a.romero0303@gmail.com>
2023-12-23 16:01:50 -0800

    finished day2 part 1

--- 2023/day2/day2.java
+++ 2023/day2/day2.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 public class day2 {
     public static int finalAnswer = 0;
     public static void main(String[] args){
-        //String file = "./2023/day2/day2input.txt";
+        String file = "./2023/day2/day2input.txt";
 
         // test file
-        String file = "./2023/day2/day2input-test.txt";
+        // String file = "./2023/day2/day2input-test.txt";
 
         try(BufferedReader reader = new BufferedReader(new FileReader(file))){
             String line = "";
@@ -19,44 +19,59 @@ public class day2 {
         }catch(IOException e){
             System.out.println("error: " + e);
         }
-        // System.out.println(finalAnswer);
+        System.out.println(finalAnswer);
 
     }
 
     public static void processLine(String fileLine){
-        int id = 0;
-        int redCount = 0;
-        int blueCount = 0;
-        int greenCount = 0;
-        int red = 12;
-        int green = 13;
-        int blue = 14;
-        String text = "";
-
         // splitting everything into two sections. splitting in between the colon
         String[] sections = fileLine.split(":");
 
-        // sections[0] is everything before the colon, and only grabbing the id number. then making it into int
         String numBefore = sections[0].replaceAll("[^0-9]", "");
 
-        id = Integer.parseInt(numBefore);
+        int id = Integer.parseInt(numBefore);
 
-        // sections[1] is everything after the colon
         String numsAfter = sections[1];
+        processGame(id, numsAfter);
+    }
 
-        String[] numbers = numsAfter.split("\n"); 
-        
-        // grabbing each game's stats 
-        for(int i = 0; i < numbers.length; i++){
-            String game = numbers[i];
+    public static void processGame(int id, String numsAfter){
+        String[] game = numsAfter.split("\n");
+        String gameNums = game[0];
+        boolean check = true;
+
+        String[] gamePulls = gameNums.split(";");
+
+        for(String m : gamePulls){
+            if(check == false){
+                break;
+            }
+
+            String[] pulls = m.split(",");
             
+            for(String a : pulls){
+                String[] beforeSemi = a.split(" ");
+                String colorNums = beforeSemi[1];
+                int colorNum = Integer.parseInt(colorNums);
+                String color = beforeSemi[2];    
+
+                if(colorNum > 12 && color.contains("red")){
+                    id = 0;
+                    check = false;
+                    break;
+                }
+                else if(colorNum > 13 && color.contains("green")){
+                    id = 0;
+                    check = false;
+                    break;
+                }
+                else if(colorNum > 14 && color.contains("blue")){
+                    id = 0;
+                    check = false;
+                    break;
+                }
             }
         }
-        
-        // if red count > red asked || blue count > blue asked || green count > green asked
-        // make id = 0 so we can add nothing to final id 
-
-        // finalAnswer += id
-        
+        finalAnswer += id;
     }
 }
