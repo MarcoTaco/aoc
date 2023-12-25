82f2e13d343099d4c528e8c1b422427effb4d807
Marco <marco.a.romero0303@gmail.com>
2023-12-25 01:41:14 -0800

    starting off day3

---
+++ 2023/day3/day3.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class day3 {
+    public static int finalAnswer = 0;
+    public static void main(String[] args){
+        
+    }
+}
