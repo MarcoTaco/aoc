6c91ffede8fe630c1c1f7f94e1f178a2fb2ab455
Marco <marco.a.romero0303@gmail.com>
2023-12-26 13:03:57 -0800

    can go through each line and grab whole numbers now

--- 2023/day3/day3.java
+++ 2023/day3/day3.java
@@ -1,29 +1,22 @@
-/*
- * for this day problem, i want to use a breadth first search, so i will need to look into how to
- * use one of those lol.
- * i know i will need to add all these lines into an array to start
- */
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class day3 {
     public static int finalAnswer = 0;
+    public static ArrayList<Integer> finalNumbers = new ArrayList<Integer>();
+
     public static void main(String[] args){
         String file = "./2023/day3/day3input.txt";
 
         String testFile = "./2023/day3/day3input-test.txt";
-        String[] testArray = new String[10];
-        int count = 0;
 
         try(BufferedReader reader = new BufferedReader(new FileReader(testFile))){
             String line = "";
 
             while((line = reader.readLine()) != null){
-                // adding lines to array
-                testArray[count] = line;
-                count++;
+                processLine(line);
             }
         }catch(IOException e){
             System.out.println("Error: " + e);
@@ -31,7 +24,24 @@ public class day3 {
         
     }
 
-    public static void lineToArray(String line){
+    public static void processLine(String line){
+        String tempString = "";
+        String stringNum = "";
+        int num = 0;
+        
+        for(int i = 0; i < line.length(); i++){
+            char letter = line.charAt(i);
+            tempString += letter;
 
+            if(Character.isDigit(letter)){
+                stringNum += Character.toString(letter);
+            }
+            else if(!Character.isDigit(letter) && stringNum != ""){
+                num = Integer.parseInt(stringNum);
+                finalNumbers.add(num);
+                stringNum = "";
+            }
+        }
+        System.out.println(finalNumbers);
     }
 }
