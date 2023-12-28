8d2b2e37dc2a939c614393fc670ec3718b946148
Marco <marco.a.romero0303@gmail.com>
2023-12-27 23:29:45 -0800

    i hate day 3 so far

--- 2023/day3/day3.java
+++ 2023/day3/day3.java
@@ -1,3 +1,8 @@
+/*
+ * loop through first row, check if special char is next to num go next line. store special char indexes
+ * loop through second row, check if special char is next to num, check if next to above special chars. store specal chars
+ * loop through third row same, check if top row is char and bottom and current. 
+ */
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,8 +14,10 @@ public class day3 {
     public static ArrayList<Integer> specialCharIndex = new ArrayList<Integer>();
 
     public static void main(String[] args){
+        // length of each line is 100
         String file = "./2023/day3/day3input.txt";
 
+        // length of each line is 10
         String testFile = "./2023/day3/day3input-test.txt";
 
         try(BufferedReader reader = new BufferedReader(new FileReader(testFile))){
