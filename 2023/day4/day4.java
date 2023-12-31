33b5948cc7b8b3a452a6386d241d372c02f301fb
Marco <marco.a.romero0303@gmail.com>
2023-12-30 21:32:27 -0800

    finished day4 part 1

--- 2023/day4/day4.java
+++ 2023/day4/day4.java
@@ -13,7 +13,7 @@ public class day4 {
 
         int finalAnswer = 0;
 
-        try(BufferedReader reader = new BufferedReader(new FileReader(testFile))){
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))){
             String line = "";
 
             while((line = reader.readLine()) != null){
@@ -38,38 +38,25 @@ public class day4 {
         HashMap<String, Integer> winningMap = new HashMap<String, Integer>();
         
         int count = 0;
-        int space = 0;
-
-        /*
-         * THIS IS A NOTE: !!!!
-         * i figured the arrays are storing some spaces for the numbers that only have 1 digit. i will need to fix that when
-         * i get back
-         */
+        int num = 0;
 
         String[] winNums = winningNums.split(" ");
         String[] playNums = playingNums.split(" ");
         
         for(String s : winNums){
-            if(space == 0){
+            if(s.isEmpty()){
 
             }
             else{
                 winningMap.put(s, 0);
             }
-            space++;
-            System.out.println("t: " + s);
         }
-        // System.out.println(winningMap);
 
         for(int i = 0; i < playNums.length; i++){
             if(winningMap.get(playNums[i]) != null){
-                System.out.println(playNums[i]);
                 count++;
             }
         }
-        // System.out.println("Card count: " + count);
-        
-        int num = 0;
 
         for(int i = 1; i <= count; i++){
             if(i < 2){
@@ -79,7 +66,7 @@ public class day4 {
                 num *= 2;
             }
         }
-        // System.out.println("game: " + num);
+
         finalCount = num;
     }
 }
