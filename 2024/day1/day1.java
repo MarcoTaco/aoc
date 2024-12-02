3fe3c023728c1e3960af3385a474142239bde58d
Marco <marco.a.romero0303@gmail.com>
2024-12-02 00:38:54 -0800

    working on part 2 of day 1

--- 2024/day1/day1.java
+++ 2024/day1/day1.java
@@ -1,9 +1,12 @@
 /*
  * part 1: im going to read through each line and add each line to an array of left and right
- * after reading through all ids, i will call another function to handle the sorting and adding
- * functionality then get a final answer.
+ *  after reading through all ids, i will call another function to handle the sorting and adding
+ *  functionality then get a final answer.
  * 
- * part 2: 
+ * part 2: add each number to a hashmap and count how many times it appears. then loop through each
+ *  map and check if it exists in both, if it doesn't, then add 0 to the array. if it does, multiply.
+ *  BUT if leftMap happens more than once, loop through the count and just add the answer multiple times
+ *  to array. at the end, add everything in the array for the final answer.
  */
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -16,24 +19,27 @@ import java.util.List;
 public class day1{
     // value of final answer
     static int finalAnswer = 0;
-
-    // using lists since the size of the list is changing and idk what sizes it would be.
+    // part 1: using lists since the size of the list is changing and idk what sizes it would be.
     static List<Integer> leftList = new ArrayList<>();
     static List<Integer> rightList = new ArrayList<>();
 
-    HashMap<Integer, Integer> leftMap = new HashMap<Integer, Integer>();
+    // part 2: using list to add each score, and adding it all up in the end.
+    static List<Integer> sums = new ArrayList<>();
+
+    static HashMap<Integer, Integer> leftMap = new HashMap<Integer, Integer>();
+    static HashMap<Integer, Integer> rightMap = new HashMap<Integer, Integer>();
     
     public static void main(String[] args){
-        String filePath = "./2024/day1/day1-input.txt";
+        // String filePath = "./2024/day1/day1-input.txt";
 
         // testing file
-        // String testFilePath = "./2024/day1/day1-input-test.txt";
+        String filePath = "./2024/day1/day1-input-test.txt";
 
         try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
             String line = "";
 
             while((line = reader.readLine()) != null){
-                // addArrayElements(line);
+                addArrayElements(line);
             }
         } catch(IOException e){
             System.out.println("error" + e);
@@ -43,12 +49,17 @@ public class day1{
         Collections.sort(leftList);
         Collections.sort(rightList);
 
+        addHashElements(leftList, rightList);
+
         // calling function for day 1 part 1 calculations
         // locationIdsPartOne(leftList, rightList);
-        
-        System.out.println(finalAnswer);
-    }
 
+        // calling function for day 1 part 2 calculations
+        locationsIdsPartTwo(leftMap, rightMap);
+        
+        // System.out.println(finalAnswer); 
+    }
+    // part 1
     private static void addArrayElements(String line){
         // splitting the left and right by one or more spaces
         String[] numbers = line.trim().split("\\s+");
@@ -70,5 +81,34 @@ public class day1{
 
             finalAnswer = finalAnswer + diffyWiffy;
         }
+    }
+
+    // part 2
+    // adding each list to a hashmap so we can keep count of the amount of times a repeatings
+    // key happened
+    private static void addHashElements(List<Integer> leftList, List<Integer> rightList){
+        // looping through both left and right list. then putting each value in a map and checking
+        // if a value is already there
+        // getOrDefault is get the count, then add by 1, or assign a default value of 1.
+        for(Integer leftNum : leftList){
+            leftMap.put(leftNum, leftMap.getOrDefault(leftNum, 0) + 1);
+        }
+        for(Integer rightNum : rightList){
+            rightMap.put(rightNum, rightMap.getOrDefault(rightNum, 0) + 1);
+        }
+    }
+
+    private static void locationsIdsPartTwo(HashMap<Integer, Integer> leftMap, HashMap<Integer, Integer> rightMap){
+        for(Integer num : leftMap.keySet()){
+            if(rightMap.containsKey(num)){
+                // here we check how many time it occurs on right map and multiply it by that many times
+                int repeatNums = rightMap.get(num);
+                for(int i = 0; i < repeatNums; i++){
+                    int sum = num * num;
+                    finalAnswer = finalAnswer + sum;
+                }
+            }
+        }
+        System.out.println(finalAnswer);
     }
 }
