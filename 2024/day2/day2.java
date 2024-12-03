7e9273c0d19145134da3fccd8c64c0820d64bbad
Marco <marco.a.romero0303@gmail.com>
2024-12-02 23:11:05 -0800

    finished day 2 part 1

--- 2024/day2/day2.java
+++ 2024/day2/day2.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 public class day2{
     // final answers for both part one and two
@@ -12,20 +13,60 @@ public class day2{
     static List<Integer> lineOfNums = new ArrayList<>();
 
     public static void processLinePartOne(String line){
-        String[] numbers = line.trim().split("\\s+");
+        int[] nums = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();        
 
-        for(int i = 0; i < numbers.length; i++){
-            lineOfNums.add(Integer.parseInt(numbers[i]));
+        // adding nums array to list so i can clear the list after im done with calculation
+        for(Integer n : nums){
+            lineOfNums.add(n);
         }
 
-        System.out.println(lineOfNums);
-        lineOfNums.clear();
+        // im going to use two pointers to compare the two numbers
+        int start = 0;
+        int end = 1;
+
+        if(lineOfNums.get(start) > lineOfNums.get(end)){
+            for(int i = 1; i < lineOfNums.size(); i++){
+                int check = lineOfNums.get(start) - lineOfNums.get(i);
+
+                if((check <= 3 && check >= 1) && (lineOfNums.get(start) > lineOfNums.get(i))){
+                    start++;
+                    continue;
+                }
+                else{
+                    lineOfNums.clear();
+                    break;
+                }
+            }
+        }
+        else if(lineOfNums.get(start) < lineOfNums.get(end)){
+            for(int i = 1; i < lineOfNums.size(); i++){
+                int check = lineOfNums.get(start) - lineOfNums.get(i);
+                check = Math.abs(check);
+                
+                if((check <= 3 && check >= 1) && (lineOfNums.get(start) < lineOfNums.get(i))){
+                    start++;
+                    continue;
+                }
+                else{
+                    lineOfNums.clear();
+                    break;
+                }
+            }
+        }
+        else{
+            lineOfNums.clear();
+        }
+        
+        if(lineOfNums.size() != 0){
+            finalAnswerPartOne++;
+            lineOfNums.clear();
+        }
     }
 
     public static void main(String[] args){
-        String filePath = "./2024/day2/day2-input-test.txt";
+        // String filePath = "./2024/day2/day2-input-test.txt";
 
-        // String filePath = "./2024/day2/day2-input.txt"
+        String filePath = "./2024/day2/day2-input.txt";
 
         try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
             String line = "";
@@ -36,5 +77,7 @@ public class day2{
         } catch(IOException e){
             System.out.println("Error reading file: " + e);
         }
+
+        System.out.println(finalAnswerPartOne);
     }
 }
