0e4509ed03898e30319fabb0a1d0843cf8597471
Marco <marco.a.romero0303@gmail.com>
2024-12-07 18:45:56 -0800

    layout of day 5

---
+++ 2024/day5/day5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class day5 {
+    static int finalAnswerPartOne = 0;
+    static int finalAnswerPartTwo = 0;
+
+    
+    public static void main(String[] args){
+
+    }
+}
