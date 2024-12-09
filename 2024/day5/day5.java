0f4f4a3819213a0c4e773652563b607aa40c5f99
Marco <marco.a.romero0303@gmail.com>
2024-12-08 18:32:58 -0800

    process both first and second sections now. process on day 5 part 1

--- 2024/day5/day5.java
+++ 2024/day5/day5.java
@@ -3,10 +3,62 @@ import java.io.*;
 
 public class day5 {
     static int finalAnswerPartOne = 0;
-    static int finalAnswerPartTwo = 0;
-
+    // static int finalAnswerPartTwo = 0;
+    static List<String> firstSection = new ArrayList<>();
+    static List<List<Integer>> pageNums = new ArrayList<>();
     
-    public static void main(String[] args){
+    // this function is for making a 2d list to have a collection of all pages
+    public static List<List<Integer>> processFirstSection(List<String> firstSection){
+        List<List<Integer>> pages = new ArrayList<>();
+        
+        for(String s : firstSection){
+            List<Integer> row = new ArrayList<>();
 
+            String[] pageOrder = s.split("\\|");
+
+            int firstNum = Integer.parseInt(pageOrder[0]);
+            int secondNum = Integer.parseInt(pageOrder[1]);
+
+            row.add(firstNum);
+            row.add(secondNum);
+
+            pages.add(row);
+        }
+        return pages;
+    }
+    // this function is for making the second section into int lists instead of lists of strings
+    public static void processEachSecondSection(List<List<Integer>> pageNums, String line){
+        List<Integer> pageToCheck = new ArrayList<>();
+
+        String[] commas = line.split(",");
+
+        for(String comma : commas){
+            pageToCheck.add(Integer.parseInt(comma.trim()));
+        }
+    }
+    public static void main(String[] args){
+        // String filePath = "./2024/day5/day5-input.txt";
+        String filePath = "./2024/day5/day5-input-test.txt";
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+            String line = "";
+            boolean newLine = false;
+
+            while((line = reader.readLine()) != null){
+                if(line.length() == 0){
+                    newLine = true;
+                    pageNums = processFirstSection(firstSection);
+                }
+                if(!newLine){
+                    firstSection.add(line);
+                }
+                else if(newLine && line.length() != 0){
+                    processEachSecondSection(pageNums, line);
+                }
+            }
+
+        }catch(IOException e){
+            System.out.println("Error: " + e);
+        }
     }
 }
